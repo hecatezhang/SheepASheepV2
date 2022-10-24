76b1d4ced793614010bdfbaf9fa7b322f97324f7
Chen Yang <adamyoung1234@outlook.com>
2022-10-24 10:02:35 -0500

    add batch mode

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("每日挑战请运行 node challenge.js , 今日话题请运行 node topic.js");
+const spawn = require("child_process").spawn;
+
+const spawnSolverProcess = (type, token) => {
+  return new Promise((resolve) => {
+    const solverProcess = spawn("node", [`${type}.js`, "-t", token]);
+    solverProcess.stdout.on("data", (data) => {
+      const outputs = data
+        .toString()
+        .split(/\r?\n/)
+        .filter((e) => e);
+
+      for (line of outputs) {
+        console.log(line);
+      }
+    });
+
+    solverProcess.stderr.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    solverProcess.on("exit", () => {
+      resolve();
+    });
+  });
+};
+
+const main = async () => {
+  console.log("正在读取 tokens.json");
+  const tokens = require("./tokens.json");
+
+  for (id in tokens) {
+    console.log("=========================");
+    console.log("开始", id, "每日挑战");
+    console.log("=========================");
+    await spawnSolverProcess("challenge", tokens[id]);
+
+    console.log("=========================");
+    console.log("开始", id, "每日话题");
+    console.log("=========================");
+    await spawnSolverProcess("challenge", tokens[id]);
+  }
+};
+
+main();
